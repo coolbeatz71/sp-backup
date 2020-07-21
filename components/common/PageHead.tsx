1363362e4ab0ef68e8f971f2e06221813921074a
Olivier M. Esuka <oesukam@gmail.com>
2020-07-21 12:01:48 +0200

    add meta image

--- components/common/PageHead.tsx
+++ components/common/PageHead.tsx
@@ -14,10 +14,15 @@ const PageHead: React.FC<PageHeadProps> = ({ data }) => {
       <meta property="og:type" content="website" />
       <meta property="description" content={data.description} />
       <meta property="og:title" content={data.name} />
-      <meta property="description" content={data.description} />
+      <meta property="description" content={data.summary} />
       <meta property="og:url" content={`${getPlatformUrl()}/causes/${data.slug}`} />
-      <meta property="og:description" content={data.description} />
+      <meta property="og:description" content={data.summary} />
       <meta property="og:image" content={data.image} />
+
+      <meta name="twitter:title" content={data.name} />
+      <meta name="twitter:description" content={data.summary} />
+      <meta name="twitter:image" content={data.image} />
+      <meta name="twitter:card" content={data.image} />
     </Head>
   );
 };
