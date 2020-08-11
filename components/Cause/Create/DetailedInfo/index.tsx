8e355c99d95ab3fd84cbaba80a3673ad4612b85f
hadad <hadadus@gmail.com>
2020-08-11 14:53:52 +0200

    add video on cause

--- components/Cause/Create/DetailedInfo/index.tsx
+++ components/Cause/Create/DetailedInfo/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Switch } from "antd";
-import { TextArea } from "components/common/Input";
+import ReactPlayer from "react-player";
+import { TextArea, Input } from "components/common/Input";
 import isLocation from "helpers/isLocation";
 
 export interface DetailedInfoProps {}
@@ -14,21 +15,58 @@ const DetailedInfo: React.FC<DetailedInfoProps> = () => {
         validateTrigger={["onSubmit", "onBlur"]}
         rules={[{ required: true, max: 280, min: 3 }]}
       >
-        <TextArea disabled={isEditing} maxLength={280} placeholder="Summary of this Cause. " />
+        <TextArea
+          disabled={isEditing}
+          maxLength={280}
+          placeholder="Summary of this Cause. "
+        />
+      </Form.Item>
+      <Form.Item
+        name="video"
+        validateTrigger={["onSubmit", "onBlur"]}
+        rules={[
+          { max: 1000 },
+          () => ({
+            validator(_, value) {
+              if (!value || ReactPlayer.canPlay(value)) {
+                return Promise.resolve();
+              }
+              return Promise.reject(
+                "Should be a valid video url"
+              );
+            },
+          })
+        ]}
+      >
+        <Input
+          disabled={isEditing}
+          placeholder="Video url"
+          suffix={
+            <img
+              src="/icons/play-video.svg"
+              className="video-url"
+              alt="play video"
+            />
+          }
+        />
       </Form.Item>
       <Form.Item
         name="description"
         validateTrigger={["onSubmit", "onBlur"]}
         rules={[{ required: true, max: 1000, min: 3 }]}
       >
-        <TextArea disabled={isEditing} rows={6}  maxLength={1000} placeholder="Details about the cause" />
+        <TextArea
+          disabled={isEditing}
+          rows={6}
+          maxLength={1000}
+          placeholder="Details about the cause"
+        />
       </Form.Item>
       <div className="d-flex justify-content-between my-4">
-        <span className="font-weight-bold">Is this cause afflilated to any Organization/NGO </span>
-        <Form.Item
-          name="affiliated"
-          valuePropName="checked"
-        >
+        <span className="font-weight-bold">
+          Is this cause afflilated to any Organization/NGO{" "}
+        </span>
+        <Form.Item name="affiliated" valuePropName="checked">
           <Switch disabled={isEditing} />
         </Form.Item>
       </div>
