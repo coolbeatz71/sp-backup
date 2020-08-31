3ce6e933fe5046c5f900a9850f57fb1e071b3587
MutabaziAlleluia <mutabazialleluia@gmail.com>
2020-08-31 11:01:56 +0200

    Top menu responsiveness

---
+++ components/LayoutWrapper/MobileHeader.tsx
@@ -0,0 +1,264 @@
+import React from "react";
+import {
+  Layout,
+  Row,
+  Col,
+  Menu,
+  Button,
+  Avatar,
+  Typography,
+  Skeleton,
+  Dropdown,
+} from "antd";
+import logout from "redux/actions/Auth/logout";
+import _ from "lodash";
+import showAuthDialog from "redux/actions/Auth/showAuthDialog";
+
+import styles from "./index.module.scss";
+import { DownOutlined, MenuOutlined, PlusOutlined } from "@ant-design/icons";
+import { Iuser } from "redux/initialStates/user";
+
+import SignUp from "components/modals/SignUp";
+import SignIn from "components/modals/SignIn";
+import ResetPin from "components/modals/ResetPin";
+import ChangePin from "components/modals/ChangePin";
+
+import CategoryBar from "./CategoryBar";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
+import { SvpType } from "helpers/context";
+
+const { Header: GenericHeader } = Layout;
+
+interface Props {
+  scrolled: string;
+  isCategory: boolean;
+  user: Iuser;
+  isHome: boolean;
+  isCreate: boolean;
+  svpProps: SvpType;
+  baseUrl?: string;
+}
+
+const Header: React.FC<Props> = ({
+  scrolled,
+  isCategory,
+  user,
+  isHome,
+  isCreate,
+  svpProps,
+  baseUrl,
+}) => {
+  const router = useRouter();
+  const [changePin, setChangePin] = React.useState(false);
+  const dispatch = useDispatch();
+
+  const [visible, setVisible] = React.useState(false);
+
+  return (
+    <GenericHeader
+      className={styles.layout__header}
+      data-scroll={scrolled}
+      data-is-category={isCategory}
+      data-is-mobile="true"
+    >
+      <Row
+        justify="space-between"
+        align="middle"
+        className={styles.layout__header__row}
+      >
+        <Col>
+          <Link href="/">
+            <a>
+              <img
+                src="/images/logo-beta.svg"
+                className={styles.layout__header__row__logo}
+              />
+            </a>
+          </Link>
+        </Col>
+        <Col>
+          <Row gutter={24} align="middle">
+            {user.currentUser.isLoggedin &&
+              !isCreate &&
+              user.currentUser.data.id && (
+                <Col>
+                  <Button
+                    type="primary"
+                    size="small"
+                    ghost
+                    onClick={() => router.push("/causes/create")}
+                    data-create-button={!isHome ? "over" : scrolled}
+                    icon={<PlusOutlined />}
+                  />
+                </Col>
+              )}
+
+            <Col>
+              {user.currentUser.isLoggedin && user.currentUser.data.id && (
+                <ChangePin
+                  visible={changePin}
+                  onVisible={() => setChangePin(true)}
+                  onCancel={() => setChangePin(false)}
+                />
+              )}
+              {!user.currentUser.isLoggedin && (
+                <>
+                  <SignIn />
+                  <SignUp />
+                  <ResetPin />
+                </>
+              )}
+              <Dropdown
+                placement="bottomRight"
+                visible={visible}
+                onVisibleChange={(v) => setVisible(v)}
+                overlay={
+                  <Menu
+                    className={styles.layout__header__row__menu}
+                    onClick={({ key }) => {
+                      setVisible(false);
+                      if (key === "sign-out") {
+                        return logout(router.push, dispatch);
+                      }
+
+                      if (key === "change-pin") {
+                        return setChangePin(true);
+                      }
+
+                      if (key) router.push(`${key}`);
+                    }}
+                  >
+                    {!isHome && <Menu.Item key="/">Home</Menu.Item>}
+                    {svpProps.categories && svpProps.categories.length > 0 && (
+                      <Menu.SubMenu title={<span>Causes</span>}>
+                        {svpProps.categories.map((category) => (
+                          <Menu.Item key={`/causes?category_id=${category.id}`}>
+                            {category.title}
+                          </Menu.Item>
+                        ))}
+                      </Menu.SubMenu>
+                    )}
+                    <Menu.Item key="/pricing">Pricing</Menu.Item>
+                    {user.currentUser.isLoggedin && !user.currentUser.data.id && (
+                      <Menu.SubMenu
+                        className={styles.layout__header__row__user}
+                        popupClassName="header-row-user"
+                        title={
+                          <span>
+                            <Skeleton.Avatar
+                              className={
+                                styles.layout__header__row__user__avatar
+                              }
+                              active
+                              size={48}
+                            />
+                          </span>
+                        }
+                      >
+                        <div />
+                      </Menu.SubMenu>
+                    )}
+                    {user.currentUser.isLoggedin &&
+                      user.currentUser.data.id && <Menu.Divider />}
+                    {user.currentUser.isLoggedin && user.currentUser.data.id && (
+                      <Menu.SubMenu
+                        className={styles.layout__header__row__user}
+                        popupClassName="header-row-user"
+                        title={
+                          <span>
+                            <Avatar
+                              className={
+                                styles.layout__header__row__user__avatar
+                              }
+                              src={user.currentUser.data.avatar}
+                              size={32}
+                            >
+                              {`${user.currentUser.data.first_name} ${user.currentUser.data.last_name}`
+                                ?.split(" ")
+                                .map(
+                                  (n: string) => n && n.charAt(0).toUpperCase(),
+                                )}
+                            </Avatar>
+                            <Typography.Text strong ellipsis>
+                              {user.currentUser.data.first_name}
+                              {` `}
+                              {user.currentUser.data.last_name}
+                            </Typography.Text>
+                          </span>
+                        }
+                      >
+                        <Menu.Item key="/profile">Profile</Menu.Item>
+                        <Menu.Item key="/causes/create">New Cause</Menu.Item>
+                        <Menu.Item key="/user/causes">My Causes</Menu.Item>
+                        <Menu.Item key="change-pin">Change PIN</Menu.Item>
+                        <Menu.Divider />
+                        <Menu.Item key="sign-out" danger>
+                          Sign Out
+                        </Menu.Item>
+                      </Menu.SubMenu>
+                    )}
+                    {!user.currentUser.isLoggedin && (
+                      <>
+                        <div
+                          className={styles.layout__header__row__menu__sign_in}
+                        >
+                          <Button
+                            type="primary"
+                            ghost
+                            block
+                            onClick={() => {
+                              setVisible(false);
+                              showAuthDialog(true, "login")(dispatch);
+                            }}
+                          >
+                            SIGN IN
+                          </Button>
+                        </div>
+                        <div
+                          className={styles.layout__header__row__menu__sign_up}
+                        >
+                          <Button
+                            type="primary"
+                            block
+                            onClick={() => {
+                              setVisible(false);
+                              showAuthDialog(true, "signup")(dispatch);
+                            }}
+                          >
+                            SIGN UP
+                          </Button>
+                        </div>
+                      </>
+                    )}
+                  </Menu>
+                }
+                trigger={["click"]}
+              >
+                <Button type="text" size="large" icon={<MenuOutlined />} />
+              </Dropdown>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      {isCategory && (
+        <Row
+          className={styles.layout__header__row__category}
+          align="middle"
+          data-row-category
+        >
+          <Col span={24}>
+            <CategoryBar
+              categories={svpProps.categories}
+              baseUrl={baseUrl}
+              scrolled={scrolled}
+            />
+          </Col>
+        </Row>
+      )}
+    </GenericHeader>
+  );
+};
+
+export default Header;
--- components/LayoutWrapper/index.tsx
+++ components/LayoutWrapper/index.tsx
@@ -1,39 +1,25 @@
 import React from "react";
-import {
-  Layout,
-  Row,
-  Col,
-  Menu,
-  Button,
-  Result,
-  Avatar,
-  Typography,
-  Skeleton,
-} from "antd";
+import { Layout, Button, Result, Grid } from "antd";
 import Head from "next/head";
 import Context from "helpers/context";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import { IRootState } from "redux/initialStates";
 import getCurrentUser from "redux/actions/user/getCurrentUser";
-import logout from "redux/actions/Auth/logout";
 import _ from "lodash";
-import showAuthDialog from "redux/actions/Auth/showAuthDialog";
+
+import { HomeOutlined } from "@ant-design/icons";
 
 import FooterItem from "./FooterItem";
 
 import styles from "./index.module.scss";
-import { DownOutlined, HomeOutlined } from "@ant-design/icons";
 
-import SignUp from "components/modals/SignUp";
-import SignIn from "components/modals/SignIn";
-import ResetPin from "components/modals/ResetPin";
-import ChangePin from "components/modals/ChangePin";
+import Header from "./Header";
+import MobileHeader from "./MobileHeader";
 
-import CategoryBar from "./CategoryBar";
-import Link from "next/link";
+const { useBreakpoint } = Grid;
 
-const { Header, Footer, Content } = Layout;
+const { Footer, Content } = Layout;
 
 interface Props {
   children:
@@ -63,12 +49,11 @@ const LayoutWrapper: React.FC<Props> = ({
 }) => {
   const { svpProps } = React.useContext(Context);
   const router = useRouter();
+  const screens = useBreakpoint();
 
   const [scrolled, setScrolled] = React.useState("");
 
   const user = useSelector((state: IRootState) => state.user);
-
-  const [changePin, setChangePin] = React.useState(false);
 
   const dispatch = useDispatch();
 
@@ -105,185 +90,27 @@ const LayoutWrapper: React.FC<Props> = ({
           rel="stylesheet"
         />
       </Head>
-      {/* Start Header */}
-      <Header
-        className={styles.layout__header}
-        data-scroll={scrolled}
-        data-is-category={isCategory}
-      >
-        <Row
-          justify="space-between"
-          align="middle"
-          className={styles.layout__header__row}
-        >
-          <Col>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/logo-beta.svg"
-                  className={styles.layout__header__row__logo}
-                />
-              </a>
-            </Link>
-          </Col>
-          <Col>
-            <Row gutter={24} align="middle">
-              {user.currentUser.isLoggedin &&
-                !isCreate &&
-                user.currentUser.data.id && (
-                  <Col>
-                    <Button
-                      type="primary"
-                      ghost
-                      onClick={() => router.push("/causes/create")}
-                      data-create-button={!isHome ? "over" : scrolled}
-                    >
-                      Create a cause
-                    </Button>
-                  </Col>
-                )}
-              <Col>
-                {user.currentUser.isLoggedin && user.currentUser.data.id && (
-                  <ChangePin
-                    visible={changePin}
-                    onVisible={() => setChangePin(true)}
-                    onCancel={() => setChangePin(false)}
-                  />
-                )}
-                <Menu
-                  mode="horizontal"
-                  className={styles.layout__header__row__menu}
-                  onClick={({ key }) => {
-                    if (key === "sign-out") {
-                      return logout(router.push, dispatch);
-                    }
-
-                    if (key === "change-pin") {
-                      return setChangePin(true);
-                    }
-
-                    if (key) router.push(`${key}`);
-                  }}
-                >
-                  {!isHome && <Menu.Item key="/">Home</Menu.Item>}
-                  {svpProps.categories && svpProps.categories.length > 0 && (
-                    <Menu.SubMenu
-                      title={
-                        <span>
-                          Causes <DownOutlined />
-                        </span>
-                      }
-                    >
-                      {svpProps.categories.map((category) => (
-                        <Menu.Item key={`/causes?category_id=${category.id}`}>
-                          {category.title}
-                        </Menu.Item>
-                      ))}
-                    </Menu.SubMenu>
-                  )}
-                  <Menu.Item key="/pricing">Pricing</Menu.Item>
-                  {user.currentUser.isLoggedin && !user.currentUser.data.id && (
-                    <Menu.SubMenu
-                      className={styles.layout__header__row__user}
-                      popupClassName="header-row-user"
-                      title={
-                        <span>
-                          <Skeleton.Avatar
-                            className={styles.layout__header__row__user__avatar}
-                            active
-                            size={48}
-                          />
-                        </span>
-                      }
-                    >
-                      <div />
-                    </Menu.SubMenu>
-                  )}
-                  {user.currentUser.isLoggedin && user.currentUser.data.id && (
-                    <Menu.SubMenu
-                      className={styles.layout__header__row__user}
-                      popupClassName="header-row-user"
-                      title={
-                        <span>
-                          <Avatar
-                            className={styles.layout__header__row__user__avatar}
-                            src={user.currentUser.data.avatar}
-                            size={48}
-                          >
-                            {`${user.currentUser.data.first_name} ${user.currentUser.data.last_name}`
-                              ?.split(" ")
-                              .map(
-                                (n: string) => n && n.charAt(0).toUpperCase(),
-                              )}
-                          </Avatar>
-                          <DownOutlined />
-                        </span>
-                      }
-                    >
-                      <Menu.Item key="/profile">
-                        <Typography.Text strong ellipsis>
-                          {user.currentUser.data.first_name}
-                          {` `}
-                          {user.currentUser.data.last_name}
-                        </Typography.Text>
-                      </Menu.Item>
-                      <Menu.Divider />
-                      <Menu.Item key="/profile">Profile</Menu.Item>
-                      <Menu.Item key="/causes/create">New Cause</Menu.Item>
-                      <Menu.Item key="/user/causes">My Causes</Menu.Item>
-                      <Menu.Item key="change-pin">Change PIN</Menu.Item>
-                      <Menu.Divider />
-                      <Menu.Item key="sign-out" danger>
-                        Sign Out
-                      </Menu.Item>
-                    </Menu.SubMenu>
-                  )}
-                </Menu>
-              </Col>
-              {!user.currentUser.isLoggedin && (
-                <>
-                  <SignIn />
-                  <SignUp />
-                  <ResetPin />
-                  <Col>
-                    <Button
-                      type="primary"
-                      ghost
-                      onClick={() => showAuthDialog(true, "login")(dispatch)}
-                    >
-                      SIGN IN
-                    </Button>
-                  </Col>
-                  <Col>
-                    <Button
-                      type="primary"
-                      onClick={() => showAuthDialog(true, "signup")(dispatch)}
-                    >
-                      SIGN UP
-                    </Button>
-                  </Col>
-                </>
-              )}
-            </Row>
-          </Col>
-        </Row>
-        {isCategory && (
-          <Row
-            className={styles.layout__header__row__category}
-            align="middle"
-            data-row-category
-          >
-            <Col span={24}>
-              <CategoryBar
-                categories={svpProps.categories}
-                baseUrl={baseUrl}
-                scrolled={scrolled}
-              />
-            </Col>
-          </Row>
-        )}
-      </Header>
-      {/* End Header */}
+      {screens.lg ? (
+        <Header
+          scrolled={scrolled}
+          isCategory={isCategory}
+          user={user}
+          isHome={isHome}
+          isCreate={isCreate}
+          svpProps={svpProps}
+          baseUrl={baseUrl}
+        />
+      ) : (
+        <MobileHeader
+          scrolled={scrolled}
+          isCategory={isCategory}
+          user={user}
+          isHome={isHome}
+          isCreate={isCreate}
+          svpProps={svpProps}
+          baseUrl={baseUrl}
+        />
+      )}
       {svpProps.error || user.currentUser.error.message ? (
         <Content className={styles.layout__content} data-is-form="true">
           <Result
