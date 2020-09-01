c3f0babafb4331522d7ca43276c92ef9f600aab7
MutabaziAlleluia <mutabazialleluia@gmail.com>
2020-09-01 14:06:36 +0200

    Add title

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ import Icon from "components/common/CustomIcon";
 import Layout from "components/LayoutWrapper";
 
 const NotFound = () => (
-  <Layout noFooter isError>
+  <Layout title="Not Found" noFooter isError>
     <Result icon={<Icon type="404" style={{ fontSize: 240 }} />} />
   </Layout>
 );
