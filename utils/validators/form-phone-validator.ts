e256d0dfcbd2aa71d5703fcea19e5411aa33d9d4
coolbeatz71 <sigmacool@gmail.com>
2021-04-20 14:54:09 +0200

    bug(international-phone): improve the international number validator [fixes]

--- utils/validators/form-phone-validator.ts
+++ utils/validators/form-phone-validator.ts
@@ -1,6 +1,8 @@
 import { isOk } from "dev-rw-phone";
+import parsePhoneNumber, { CountryCode } from "libphonenumber-js";
 import i18n from "constants/locales";
 import { isEmpty } from "lodash";
+import { countryList } from "./../../constants/country-list";
 
 const formPhoneValidator = (name: string) => [
   { required: true, message: `${name} ${i18n.t("required")}` },
@@ -35,6 +37,18 @@ export const formWorldPhoneValidator = (name: string, countryCode: string) => [
         return Promise.reject(
           `${i18n.t("country_code")} ${i18n.t("required")}`,
         );
+      }
+
+      if (countryCode && value) {
+        const fullPhone = `${countryCode}${value}`;
+        const country = countryList.find(
+          (country) => country.dial_code === countryCode,
+        );
+        const code = country?.code as CountryCode;
+
+        return parsePhoneNumber(fullPhone, code)?.isValid()
+          ? Promise.resolve()
+          : Promise.reject(`${name} ${i18n.t("invalid")}`);
       }
 
       return Promise.resolve();
