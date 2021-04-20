ab301efd1d4801694e22dfaee997e0ee39a9d890
coolbeatz71 <sigmacool@gmail.com>
2021-04-20 17:23:37 +0200

    feat(single-cause): improve the single cause loading Skeleton [delivers]

--- components/common/Skeleton/SingleCause/index.tsx
+++ components/common/Skeleton/SingleCause/index.tsx
@@ -1,12 +1,49 @@
 import { FC } from "react";
-import { Skeleton } from "antd";
+import { Row, Col, Skeleton } from "antd";
 import styles from "./skeleton.module.scss";
 
 const SingleCauseSkeleton: FC<{}> = ({}) => {
   return (
     <div className={styles.skeleton__singleCause}>
-      <Skeleton.Button active />
-      <Skeleton active paragraph={{ rows: 5 }} />
+      <Row gutter={[24, 24]}>
+        <Col span={24} lg={15}>
+          <Skeleton.Button
+            active
+            className={styles.skeleton__singleCause__title}
+          />
+          <Skeleton.Image className={styles.skeleton__singleCause__causeImg} />
+          <Skeleton.Avatar
+            active
+            shape="circle"
+            size={72}
+            className={styles.skeleton__singleCause__avatar}
+          />
+          <Skeleton active paragraph={{ rows: 5 }} />
+          <Skeleton active paragraph={{ rows: 6 }} />
+        </Col>
+        <Col span={24} lg={9}>
+          <Skeleton.Button
+            active
+            className={styles.skeleton__singleCause__donateBtn}
+          />
+          <Skeleton.Button
+            active
+            className={styles.skeleton__singleCause__progresBar}
+          />
+          <Skeleton.Button
+            active
+            className={styles.skeleton__singleCause__share}
+          />
+          <Skeleton.Button
+            active
+            className={styles.skeleton__singleCause__share}
+          />
+          <Skeleton.Button
+            active
+            className={styles.skeleton__singleCause__donors}
+          />
+        </Col>
+      </Row>
     </div>
   );
 };
