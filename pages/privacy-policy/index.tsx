ef8fc43a292eca02bf09fa53b0071dd2d96e35e8
Elie Mugenzi <eliemugenzi@gmail.com>
2021-07-09 12:41:14 +0200

    Change supportt email on the privacy policy content

--- pages/privacy-policy/index.tsx
+++ pages/privacy-policy/index.tsx
@@ -3,8 +3,6 @@ import styles from "./policies.module.scss";
 import LayoutWrapper from "components/LayoutWrapper";
 import { useTranslation } from "react-i18next";
 
-
-
 const PoliciesPage: FC<{}> = () => {
   const { t } = useTranslation();
 
@@ -18,30 +16,33 @@ const PoliciesPage: FC<{}> = () => {
               <div>
                 <h1>{t("privacy_policy")}</h1>
               </div>
-
             </div>
           </div>
           <div className={styles.policies__container__terms}>
             <section>
               <h2>{t("personal_information")}</h2>
-              <p dangerouslySetInnerHTML={{
-                __html: t("personal_information_desc", {
-                  exuus: "<a href='https://exuus.com/' target='_blank'>https://exuus.com/</a>",
-                  getsave: "<a href='https://getsave.io/' target='_blank'>https://getsave.io/</a>",
-                  saveplus: "<a href='https://saveplus.io/' target='_blank'>https://saveplus.io/</a>"
-                })
-              }}
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: t("personal_information_desc", {
+                    exuus:
+                      "<a href='https://exuus.com/' target='_blank'>https://exuus.com/</a>",
+                    getsave:
+                      "<a href='https://getsave.io/' target='_blank'>https://getsave.io/</a>",
+                    saveplus:
+                      "<a href='https://saveplus.io/' target='_blank'>https://saveplus.io/</a>",
+                  }),
+                }}
               />
-              <p>
-                {t("personal_information_desc1")}
-              </p>
+              <p>{t("personal_information_desc1")}</p>
             </section>
 
             <section>
               <h2>{t("collection_of_personal_information")}</h2>
-              <p dangerouslySetInnerHTML={{
-                __html: t("Information_provided_desc1")
-              }} />
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: t("Information_provided_desc1"),
+                }}
+              />
               <ul>
                 <li>{t("Names")}</li>
                 <li>{t("ID_number")}</li>
@@ -72,36 +73,65 @@ const PoliciesPage: FC<{}> = () => {
                 <dd>-{t("Process transactions")}</dd>
                 <dd>-{t("For credit scoring")}</dd>
                 <dd>-{t("Provide customer support and resolve disputes")}</dd>
-                <dd>-{t("Identify, prevent, and report potentially prohibited fraudulent, or illegal activities")}</dd>
-                <dd>-{t("Customize and improve your experience and the Services")}</dd>
-                <dd>-{t("Notify you about important changes to the User Agreement terms and conditions")}</dd>
-                <dd>-{t("Any other purpose that we disclose to you in the course of providing the Services to you")}</dd>
-                <dd>-{t("We can also share your personal data with a third party entity with your consent")}</dd>
-                <dd>-{t("Personal data can also be shared with facilitating organizations such as NGOs")}</dd>
+                <dd>
+                  -
+                  {t(
+                    "Identify, prevent, and report potentially prohibited fraudulent, or illegal activities"
+                  )}
+                </dd>
+                <dd>
+                  -{t("Customize and improve your experience and the Services")}
+                </dd>
+                <dd>
+                  -
+                  {t(
+                    "Notify you about important changes to the User Agreement terms and conditions"
+                  )}
+                </dd>
+                <dd>
+                  -
+                  {t(
+                    "Any other purpose that we disclose to you in the course of providing the Services to you"
+                  )}
+                </dd>
+                <dd>
+                  -
+                  {t(
+                    "We can also share your personal data with a third party entity with your consent"
+                  )}
+                </dd>
+                <dd>
+                  -
+                  {t(
+                    "Personal data can also be shared with facilitating organizations such as NGOs"
+                  )}
+                </dd>
               </dl>
               <p>{t("Use_of_Personal_Information_last_paragraph")}</p>
             </section>
 
             <section>
               <h2>{t("Privacy_Questions")}</h2>
-              <p dangerouslySetInnerHTML={{
-                __html: t("privacy_question_description", {
-                  email: "<a href='mailto:support@getsave.io' target='_blank'>support@getsave.io</a>",
-                  email1: "<a href='mailto:info@exuus.com' target='_blank'>info@exuus.com</a>",
-                })
-              }} />
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: t("privacy_question_description", {
+                    email:
+                      "<a href='mailto:support@saveplus.io' target='_blank'>support@saveplus.io</a>",
+                    email1:
+                      "<a href='mailto:info@exuus.com' target='_blank'>info@exuus.com</a>",
+                  }),
+                }}
+              />
             </section>
 
             <section>
               <h2>{t("Update")}</h2>
               <p>{t("update_description")}</p>
             </section>
-
           </div>
         </div>
       </div>
-
-    </LayoutWrapper >
+    </LayoutWrapper>
   );
 };
 
